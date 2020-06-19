8ab99b24c0d56f15a4c1b19e21645cd1904d3516
Andre Kot <andrii.bekey@gmail.com>
2020-06-19 13:28:31 +0300

    add test suite

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>216bac5f-34b3-4bb7-b7d2-c15b1f54fc53</testSuiteGuid>
+</TestSuiteEntity>
